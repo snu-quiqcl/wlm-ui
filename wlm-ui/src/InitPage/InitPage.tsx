72c859602373ec542d2940e5458dd18c76e2d9ed
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 20:06:22 +0900

    Add `input` for username
    See also: #3

--- wlm-ui/src/InitPage/InitPage.tsx
+++ wlm-ui/src/InitPage/InitPage.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 const InitPage = () => {
+    const [username, setUsername] = useState<string>('')
     return (
         <div>
-            Init
+            <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
         </div>
     );
 };
