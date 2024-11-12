07d6aff45acc5449015201fdeafa5dd6bb4719a9
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 19:47:34 +0900

    Set routing to init and main pages
    See also: #3

--- wlm-ui/src/App.tsx
+++ wlm-ui/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import InitPage from './InitPage/InitPage';
+import MainPage from './MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/init' element={<InitPage />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
