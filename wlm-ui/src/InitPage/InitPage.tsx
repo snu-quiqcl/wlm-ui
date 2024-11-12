a2eebcee781ab9b43f3a72349d95f14f296a5f90
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 20:29:43 +0900

    Replace double quotes with single ones
    See also: #3

--- wlm-ui/src/InitPage/InitPage.tsx
+++ wlm-ui/src/InitPage/InitPage.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const InitPage = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
-    const onClickSignin = async () => {};
+    const onClickSignin = async () => {
+        if (username !== '' && password !== '') {
+            const data = { username: username, password: password };
+        }
+    };
 
     return (
         <div>
             <input
-                type="text"
-                placeholder="username"
+                type='text'
+                placeholder='username'
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
             />
             <input
-                type="password"
-                placeholder="password"
+                type='password'
+                placeholder='password'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
