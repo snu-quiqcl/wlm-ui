b89a8a271f18a34f4e3d685f77980cc6014f2bf7
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-13 16:31:26 +0900

    Add extra reducer for `signin.fulfilled`
    See also: #3

--- wlm-ui/src/store/slices/user/user.ts
+++ wlm-ui/src/store/slices/user/user.ts
@@ -31,4 +31,10 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(signin.fulfilled, (state, action) => {
+                state.user = action.payload.user;
+            })
+    }
 });
