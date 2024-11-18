35944f17ae8c56972f8b05539d13e35728b014e6
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-18 17:06:04 +0900

    Dispatch sign-out action in button listener
    See also: #8

--- wlm-ui/src/MainPage/MainPage.tsx
+++ wlm-ui/src/MainPage/MainPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { AppDispatch } from '../store';
+import { signout } from '../store/slices/user/user';
 
 const MainPage = () => {
-    const onClickSignout = async () => {};
+    const dispatch = useDispatch<AppDispatch>();
+
+    const onClickSignout = async () => {
+        dispatch(signout());
+    };
 
     return (
         <div>
