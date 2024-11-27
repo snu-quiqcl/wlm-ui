00a9aaf7b8465b63939f6decbc7ec30074cea139
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-27 19:58:03 +0900

    Move refresh button to channel list viewer
    See also: #5

--- wlm-ui/src/MainPage/Channel/ChannelList.tsx
+++ wlm-ui/src/MainPage/Channel/ChannelList.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { channelListActions, selectChannelList } from '../../store/slices/channel/channel';
+import { AppDispatch } from '../../store';
+import { channelListActions, fetchList, selectChannelList } from '../../store/slices/channel/channel';
 import Channel from './Channel';
 import './ChannelList.scss';
 
@@ -12,23 +13,30 @@ const ChannelList = () => {
         localStorage.setItem('channel.channelList', JSON.stringify(channelListState.channels));
     }, [channelListState]);
 
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>();
+
+    const onClickRefreshChannelList = async () => {
+        dispatch(fetchList());
+    };
 
     const onClickUse = (channel: number) => {
         dispatch(channelListActions.toggleUse({ channel: channel }));
     };
 
     return (
-        <section className='channel-container'>
-            {channelListState.channels.map((info) => (
-                <article key={info.channel.channel}>
-                    <Channel
-                        {...info}
-                        onClickUse={() => onClickUse(info.channel.channel)}
-                    />
-                </article>
-            ))}
-        </section>
+        <div>
+            <button onClick={onClickRefreshChannelList}>Refresh</button>
+            <section className='channel-container'>
+                {channelListState.channels.map((info) => (
+                    <article key={info.channel.channel}>
+                        <Channel
+                            {...info}
+                            onClickUse={() => onClickUse(info.channel.channel)}
+                        />
+                    </article>
+                ))}
+            </section>
+        </div>
     );
 };
 
