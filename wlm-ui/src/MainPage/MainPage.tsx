faa00745086dde9f6391e55a148421bf7b3db120
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-02 15:44:46 +0900

    Add calib. button and implement listener
    See also: #12

--- wlm-ui/src/MainPage/MainPage.tsx
+++ wlm-ui/src/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import { AppDispatch } from '../store';
 import { signout } from '../store/slices/user/user';
+import { calibrate } from '../store/slices/calibration/calibration';
 import ChannelList from './Channel/ChannelList';
 
 const MainPage = () => {
@@ -12,8 +13,13 @@ const MainPage = () => {
         dispatch(signout());
     };
 
+    const onClickCalibration = async () => {
+        dispatch(calibrate());
+    };
+
     return (
         <div>
+            <button onClick={onClickCalibration}>Calibration</button>
             <button onClick={onClickSignout}>Sign out</button>
             <ChannelList />
         </div>
