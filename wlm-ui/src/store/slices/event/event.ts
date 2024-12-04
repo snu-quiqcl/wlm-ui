40e1a9f54870d69b0a5002fde20b461378d0561e
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-04 14:05:59 +0900

    Define event list slice
    I modified the name of the channel list slice.
    
    See also: #15

--- wlm-ui/src/store/slices/event/event.ts
+++ wlm-ui/src/store/slices/event/event.ts
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export interface EventType {
     category: string;
     content: string;
@@ -11,3 +13,17 @@ export interface EventInfo {
 export interface EventListInfo {
     events: EventInfo[];
 };
+
+const initialState: EventListInfo = {
+    events: [],
+};
+
+export const EventListSlice = createSlice({
+    name: 'event',
+    initialState,
+    reducers: {},
+});
+
+export const eventListActions = EventListSlice.actions;
+
+export default EventListSlice.reducer;
