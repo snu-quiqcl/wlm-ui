5f1f56d7945438378480a8df11d4b687593e85de
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-04 16:17:48 +0900

    Add HTML components for event viewer
    See also: #15

--- wlm-ui/src/MainPage/EventList/EventList.tsx
+++ wlm-ui/src/MainPage/EventList/EventList.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { AppDispatch } from '../../store';
-import { eventListActions, EventType } from '../../store/slices/event/event';
+import { eventListActions, EventType, selectEventList } from '../../store/slices/event/event';
 import './EventList.scss';
 
 const EventList = () => {
+    const eventListState = useSelector(selectEventList);
     const dispatch = useDispatch<AppDispatch>();
 
     useEffect(() => {
@@ -21,7 +22,21 @@ const EventList = () => {
 
     return (
         <div>
-            
+            <h1>Event</h1>
+            <div className="event-container">
+                {eventListState.events.map((info, index) => {
+                    const event = info.event;
+                    const timestamp = new Date(event.occurredAt);
+
+                    return (
+                        <div key={index} className="event-component">
+                            <span>{timestamp.toLocaleString()}</span>
+                            <span>{event.category}</span>
+                            <span>{event.content}</span>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
