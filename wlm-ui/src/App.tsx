ebb08510e3f478f913cf1c13c7f2091a74f1e163
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-05 19:47:15 +0900

    Wrap app container with theme provider
    See also: #27

--- wlm-ui/src/App.tsx
+++ wlm-ui/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import './App.css';
 import InitPage from './pages/InitPage/InitPage';
@@ -9,25 +11,29 @@ import { selectUser } from './store/slices/user/user';
 
 function App() {
   const userState = useSelector(selectUser);
+  const theme = createTheme();
 
   return (
-    <div className='App'>
-      {userState.user === null ? (
-        <BrowserRouter>
-          <Routes>
-            <Route path='/init' element={<InitPage />} />
-            <Route path='*' element={<Navigate to='/init' replace />} />
-          </Routes>
-        </BrowserRouter>
-      ) : (
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='*' element={<Navigate to='/' replace />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className='App'>
+        {userState.user === null ? (
+          <BrowserRouter>
+            <Routes>
+              <Route path='/init' element={<InitPage />} />
+              <Route path='*' element={<Navigate to='/init' replace />} />
+            </Routes>
+          </BrowserRouter>
+        ) : (
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<MainPage />} />
+              <Route path='*' element={<Navigate to='/' replace />} />
+            </Routes>
+          </BrowserRouter>
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
