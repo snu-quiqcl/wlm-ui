0e240cb4efada45daf283a7fbf3836a19e0f59f2
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-09 18:09:45 +0900

    Implement slice for calibration
    See also: #27

--- wlm-ui/src/store/index.ts
+++ wlm-ui/src/store/index.ts
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user/user';
 import channelListReducer from './slices/channel/channel';
 import eventListReducer from './slices/event/event';
+import calibrationReducer from './slices/calibration/calibration';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         channelList: channelListReducer,
         eventList: eventListReducer,
+        calibration: calibrationReducer,
     },
 });
 
--- wlm-ui/src/store/slices/calibration/calibration.ts
+++ wlm-ui/src/store/slices/calibration/calibration.ts
@@ -1,5 +1,13 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+import { RootState } from '../..';
+
+export interface CalibrationInfo {
+    isCalibrated?: boolean;
+};
+
+const initialState: CalibrationInfo = {};
 
 export const calibrate = createAsyncThunk(
     'calibration',
@@ -7,3 +15,26 @@ export const calibrate = createAsyncThunk(
         await axios.post('/calibration/');
     },
 );
+
+export const calibrationSlice = createSlice({
+    name: 'calibration',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(calibrate.pending, (state) => {
+                state.isCalibrated = undefined;
+            })
+            .addCase(calibrate.fulfilled, (state) => {
+                state.isCalibrated = true;
+            })
+            .addCase(calibrate.rejected, (state) => {
+                state.isCalibrated = false;
+            })
+    },
+});
+
+export const calibrationActions = calibrationSlice.actions;
+export const selectCalibration = (state: RootState) => state.calibration;
+
+export default calibrationSlice.reducer;
