46aeec844e8a8af75c02e4b30ac1ae63a39dfea5
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-09 15:46:11 +0900

    Design channel list
    See also: #27

--- wlm-ui/src/pages/MainPage/components/ChannelList/ChannelList.tsx
+++ wlm-ui/src/pages/MainPage/components/ChannelList/ChannelList.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import AdjustIcon from '@mui/icons-material/Adjust';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 import { AppDispatch } from '../../../../store';
 import { fetchList, selectChannelList } from '../../../../store/slices/channel/channel';
+import { calibrate } from '../../../../store/slices/calibration/calibration';
 import Channel from '../Channel/Channel';
-import './ChannelList.scss';
 
 const ChannelList = () => {
     const channelListState = useSelector(selectChannelList);
@@ -18,17 +23,50 @@ const ChannelList = () => {
         dispatch(fetchList());
     };
 
+    const onClickCalibration = async () => {
+        dispatch(calibrate());
+    };
+
     return (
-        <div>
-            <button onClick={onClickRefreshChannelList}>Refresh</button>
-            <section className='channel-container'>
-                {channelListState.channels.map((info) => (
-                    <article key={info.channel.channel}>
-                        <Channel {...info} />
-                    </article>
+        <Stack spacing={2}>
+            <Stack
+                direction='row'
+                sx={{ justifyContent: 'space-between' }}
+            >
+                <Button
+                    variant='contained'
+                    size='small'
+                    endIcon={<RefreshIcon />}
+                    onClick={onClickRefreshChannelList}
+                >
+                    refresh
+                </Button>
+                <Button
+                    variant='contained'
+                    size='small'
+                    color='secondary'
+                    endIcon={<AdjustIcon />}
+                    onClick={onClickCalibration}
+                >
+                    calibrate
+                </Button>
+            </Stack>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    gap: 2,
+                }}
+            >
+                {channelListState.channels.map(info => (
+                    <Channel
+                        key={info.channel.channel}
+                        {...info}
+                    />
                 ))}
-            </section>
-        </div>
+            </Box>
+        </Stack>
     );
 };
 
