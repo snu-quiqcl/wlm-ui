331c1202c4d3c31329686f20638265f6d19c646e
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-17 15:29:13 +0900

    Set-up proxy for HTTP connection
    See also: #31

---
+++ wlm-ui/src/setupProxy.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+    }),
+  );
+};
