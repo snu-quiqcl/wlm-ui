db2592506141d2878c1e7b1f88b388b5f3d89995
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-01 13:55:25 +0900

    Assign `async`
    See also: #5

--- wlm-ui/src/MainPage/Channel/ChannelList.tsx
+++ wlm-ui/src/MainPage/Channel/ChannelList.tsx
@@ -21,15 +21,15 @@ const ChannelList = () => {
         dispatch(fetchList());
     };
 
-    const onClickUse = (channel: number) => {
+    const onClickUse = async (channel: number) => {
         dispatch(channelListActions.toggleUse({ channel: channel }));
     };
 
-    const onClickSetExposure = (channel: number, exposure: number) => {
+    const onClickSetExposure = async (channel: number, exposure: number) => {
         dispatch(postExposure({ channel: channel, exposure: exposure }));
     };
     
-    const onClickSetPeriod = (channel: number, period: number) => {
+    const onClickSetPeriod = async (channel: number, period: number) => {
         dispatch(postPeriod({ channel: channel, period: period }));
     };
 
